139c34a083f1cd83c2a69e953664f01e5a2e6ef6
triforze1-git <triforze1.git@gmail.com>
2021-10-27 17:54:54 -0400

    finished navbar

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    lopLine: 'ExclusiveAccess',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    im: 'images/svg-1.svg',
+    alt: 'Credit Card'
+}
